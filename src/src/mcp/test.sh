ad556258aa18d89d5d9f5f87b0ff3f630b1a15a6
Joe Olson <joe.olson@ibm.com>
2025-12-03 13:30:14 -0600

    Closes #175
    Signed-off-by: Joe Olson <joe.olson@ibm.com>

--- src/src/mcp/test.sh
+++ src/src/mcp/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # For local
-export NEXT_GEM_DATA_CATALOG_MCP_SERVER=http://127.0.0.1:8080/mcp
+#export NEXT_GEM_DATA_CATALOG_MCP_SERVER=http://127.0.0.1:8080/mcp
+export NEXT_GEM_DATA_CATALOG_MCP_SERVER=https://next-gem-catalog-mcp.fastmcp.app/mcp
 uv run ./test/test_next_gem_catalog.py
